928e325c938c3bf446a8af45d60d124b6b8cde34
juanluis <juanlu_40@hotmail.com>
2016-05-10 14:14:30 +0200

    php login registro previo

--- Controlador/ControlLogin.php
+++ Controlador/ControlLogin.php
@@ -1,12 +1,18 @@
 <?php
 
+
+/**
+ *Carga del objeto ModeloLogin.
+ * 
+ *Carga del objeto ModeloLogin para poder usarlo y llamar a los métodos.
+ */
 require "../vendor/autoload.php";
 use ProyectoScrum\Modelo\ModeloLogin;
 $ModeloLogin=new ModeloLogin();
 /*
- * Control de usuario y contraseña por GET
+ * Control de usuario y contraseña
  * 
- * Se controlarán los datos por GET, los cuales serán comprobados por la base
+ * Se controlarán los datos, los cuales serán comprobados por la base
  * de datos para verificarlos y en caso de error, dar un mensaje con el error.
  */
 if($_SERVER["REQUEST_METHOD"] == "GET"){
@@ -15,16 +21,35 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     echo $email;
 }
 else if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    /*
+     * Entrada de contenidos de ajax
+     */
     $datosConsulta = file_get_contents('php://input');
     
+    /*
+     * Datos del json abierto
+     */
+    
     $datos = json_decode($datosConsulta,true);
+    
+    /*
+     * variables obtenidas del json
+     * 
+     * Las variables son <var>email</var>, <var>password</var>
+     */
     $email = $datos["email"];
     $password = $datos["password"];
     
+    /*
+     * llamadas a modeloLogin para realizar las consultas, en caso de error,
+     * devuelve el mensaje de error correspondiente.
+     */
+    
     $ModeloLogin->conexionLogin();
-    $valido=$ModeloLogin->compruebaEmail($email);
+    $valido=$ModeloLogin->compruebaEmail($email,"datos");
     if($valido){
-        $valido=$ModeloLogin->compruebaPassword($password,$email);
+        $valido=$ModeloLogin->compruebaPassword($password,$email,"datos");
         if($valido){
             echo $datosConsulta;
             $ModeloLogin->cierre();
--- Modelo/ModeloLogin.php
+++ Modelo/ModeloLogin.php
@@ -6,20 +6,21 @@ namespace ProyectoScrum\Modelo;
  * Clase ModeloLogin.
  * 
  * En esta clase se realizaran los metodos de comprobacion
- *  del usuario a logear.
+ *  del usuario a logear o al registrarse.
  */
 class ModeloLogin {
     
     /**
-     * variables de el login
+     * variables de el login y Registro
      * 
      * variables para una base de datos.
      * 
      * @var Variable de logeo en base de datos <var>log</var> y Resultado
-     *  de las consultas <var>Resultado</var>.
+     *  de las consultas <var>Resultado</var>, junto con el error <var>error</var>.
      */
     private $log;
     private $Resultado;
+    private $error;
     
     /*
      * Metodo de conexion BD.
@@ -34,33 +35,101 @@ class ModeloLogin {
         }
     }
     
-    /*
+    /**
      * Metodo de comprobacion de email
      * 
-     * Este metodo nos comprueba que nuestro email existe en la base de datos o no
+     * Este metodo nos comprueba que nuestro email existe en la base de datos
+     *  o no.
+     * 
+     * @param Variables de entrada <var>email</var> y <var>tabla</var>
+     * @return devuelve si existe o no el email, y en caso de error, devuelve
+     * el error.
+     * 
      */
-    public function compruebaEmail($email){
-        $this->Resultado=$this->log->query("SELECT email FROM datos WHERE email='".$email."';");
+    public function compruebaEmail($email,$tabla){
+        try{
+        $this->Resultado=$this->log->query("SELECT email FROM ".$tabla." WHERE email='".$email."';");
         if ($this->Resultado->num_rows > 0){
             return true;
         }else{
             return false;
         }
+        } catch (\Exception $e){
+            $this->error = $e->getMessage();
+            return false;
+        }
     }
-    /*
+    
+   /**
+     * Metodo de comprobacion de Nick
+     * 
+     * Este metodo nos comprueba que nuestro Nick existe en la base de datos
+     *  o no.
+     * 
+     * @param Variables de entrada <var>Nick</var> y <var>tabla</var>
+     * @return devuelve si existe o no el Nick, y en caso de error, devuelve
+     * el error.
+     * 
+     */
+    public function compruebaNick($nick,$tabla){
+        try{
+        $this->Resultado=$this->log->query("SELECT email FROM ".$tabla." WHERE email='".$nick."';");
+        if ($this->Resultado->num_rows > 0){
+            return true;
+        }else{
+            return false;
+        }
+        } catch (\Exception $e){
+            $this->error = $e->getMessage();
+            return false;
+        }
+    }
+    
+    /**
      * Metodo de comprobar password
      * 
      * Este metodo nos comprueba que la password existe o no para ese email.
+     * 
+     * @param Variables de entrada <var>password</var>, <var>email</var> 
+     * y <var>tabla</var>
+     * @return devuelve si el email y el password es correcto password
+     * para ese email, y en caso de error, devuelve
+     * el error.
      */
-    public function compruebaPassword($password,$email){
-        $this->Resultado=$this->log->query("SELECT * FROM datos WHERE password='".$password."' AND email='".$email."';");
+    public function compruebaPassword($password,$email,$tabla){
+        try{
+        $this->Resultado=$this->log->query("SELECT * FROM ".$tabla." WHERE password='".$password."' AND email='".$email."';");
         if ($this->Resultado->num_rows > 0){
             return true;
         }else{
             return false;
         }
+        } catch (\Exception $e){
+            $this->error = $e->getMessage();
+            return false;
+        }
     }
-    /*
+    /**
+     * Metodo de insercion en base de datos
+     * 
+     * Este metodo inserta los datos del nuevo usuario en la base de datos.
+     * 
+     * @param Variables de entrada son <var>email</var>, <var>password</var>,
+     * <var>nick</var>, <var>nombre</var>, <var>apellidos</var> y la tabla a la
+     * que llama.
+     * @return completado.
+     */
+    
+    public function insertarDatosNuevos($nombre,$apellidos,$nick,$email,$password,$tabla){
+        try{
+        $this->Resultado=$this->log->query("INSERT INTO ".$tabla." VALUES ('".$nombre."','".$apellidos."','".$nick."','".$email."','".$password."');");
+        return true;
+        } catch (\Exception $e){
+            $this->error = $e->getMessage();
+            return false;
+        }
+    }
+    /**
      * Metodo de cierre
      * 
      * Este metodo cierra el log de la base de datos. 
@@ -68,13 +137,21 @@ class ModeloLogin {
     public function cierre(){
         $this->log->close();
     }
-    /*
+    /**
      * Metodo de errores
      * 
-     * Nos devuelve una cadena de error por si no coinciden los datos instroducidos.
+     * Nos devuelve una cadena de error por si no coinciden los datos
+     *  instroducidos.
+     * @param Variable de entrada <var>Tipo<var>
+     * @return Devuelve un error según el tipo que le haya entrado, o bien
+     * en caso de error general, devuelve el error de la base de datos.     
      */
     public function error($tipo){
-        $error =["error"=>"El ".$tipo." no coincide."];
+        if (Empty($this->error)){
+            $error =["error"=>"El ".$tipo." no coincide."];
+        }else{
+            $error =["error"=>$error];
+        }
         echo json_encode($error);
     }
 }
